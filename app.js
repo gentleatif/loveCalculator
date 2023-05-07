5b43235d3f566fd18c8cae4206f15749132a9f03
atifgriphhy <atif@griphhy.com>
2023-05-07 15:50:25 +0530

    added process.env for port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -55,7 +56,6 @@ app.get("/calculate", function (req, res) {
   }
   res.render("result", { data: randomNo });
 });
-
-app.listen(3000, function () {
-  console.log("server is running on port 3000");
+app.listen(PORT, function () {
+  console.log(`server is running on port 3000 ${PORT}`);
 });
